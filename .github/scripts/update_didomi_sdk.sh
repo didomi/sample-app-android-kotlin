7a0bea1943c16520734d718cbe4bd56c31e5d694
Nicolas CHAIX <80761828+nicolas-chaix-didomi@users.noreply.github.com>
2022-11-17 20:51:28 +0100

    better job for updating didomi sdk (#5)
    * better job for updating didomi sdk
    
    * disable build on push for other branches
    
    * disable build on push
    
    Co-authored-by: Automatic update <tech@didomi.io>

--- .github/scripts/update_didomi_sdk.sh
+++ .github/scripts/update_didomi_sdk.sh
@@ -4,16 +4,24 @@
 # Update Android SDK version (latest from repos)
 #----------------------------------------------------------
 
-# Update android SDK Version
-androidVersion=$(curl -s 'https://search.maven.org/solrsearch/select?q=didomi' | sed -n 's|.*"latestVersion":"\([^"]*\)".*|\1|p')
-if [[ ! $androidVersion =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
-  echo "Error while getting android SDK version ($androidVersion)"
+buildFile="app/build.gradle.kts"
+
+mavenUrl="https://repo.maven.apache.org/maven2/io/didomi/sdk/android/"
+mavenVersion=""
+
+while read -r line; do
+  version="$(echo "$line" | sed -r 's/^.+href="([^"]+)".+$/\1/' | sed 's/.$//')"
+  if [[ $version =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
+    mavenVersion="$version"
+  fi
+done <<< "$(curl -s "$mavenUrl")"
+
+if [[ ! $mavenVersion =~ ^[0-9]+.[0-9]+.[0-9]+$ ]]; then
+  echo "Error while getting local android SDK version ($mavenVersion)"
   exit 1
 fi
 
-echo "Android SDK last version is $androidVersion"
-
-sed -i~ -e "s|io.didomi.sdk:android:[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}|io.didomi.sdk:android:$androidVersion|g" app/build.gradle.kts || exit 1
+sed -i~ -e "s|io.didomi.sdk:android:[0-9]\{1,2\}.[0-9]\{1,2\}.[0-9]\{1,2\}|io.didomi.sdk:android:$mavenVersion|g" $buildFile || exit 1
 
 # Cleanup backup files
 find . -type f -name '*~' -delete
