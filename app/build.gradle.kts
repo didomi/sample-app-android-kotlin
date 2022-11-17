73c00e11e10b47a66343e8b8fe0acd0de2d67cf9
Automatic update <tech@didomi.io>
2022-11-17 17:03:09 +0100

    setup Kotlin DSL

---
+++ app/build.gradle.kts
@@ -0,0 +1,46 @@
+plugins {
+    id("com.android.application")
+    kotlin("android")
+}
+
+android {
+    buildToolsVersion = "33.0.0"
+    compileSdk = 33
+    namespace = "com.didomi.sampleappkotlin"
+
+    defaultConfig {
+        minSdk = 21
+        targetSdk = 33
+
+        versionCode = 1
+        versionName = "1.0"
+
+        applicationId = "com.didomi.sampleappkotlin"
+    }
+
+    buildTypes {
+        maybeCreate("release")
+        getByName("release") {
+            isMinifyEnabled = false
+            proguardFiles(getDefaultProguardFile("proguard-android.txt"), "proguard-rules.pro")
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+
+    kotlinOptions {
+        jvmTarget = JavaVersion.VERSION_1_8.toString()
+    }
+}
+
+dependencies {
+    implementation("io.didomi.sdk:android:1.70.2")
+
+    implementation("androidx.core:core-ktx:1.9.0")
+
+    implementation("com.google.android.gms:play-services-ads:21.3.0")
+    implementation("com.google.android.material:material:1.7.0")
+}
