48b7805e32020d11068b9630797e1fc6a3362b3b
Nicolas CHAIX <nicolas.chaix@didomi.io>
2023-01-30 11:04:57 +0000

    Update Didomi SDK dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,7 +43,7 @@ android {
 }
 
 dependencies {
-    implementation("io.didomi.sdk:android:1.72.1")
+    implementation("io.didomi.sdk:android:1.73.0")
 
     implementation("androidx.core:core-ktx:1.9.0")
 
