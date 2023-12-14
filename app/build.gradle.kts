c5d0d8767419009d8d15ebaea537ccfa0e3f8e6b
Ken L'Héritier <ken.lheritier@gmail.com>
2023-12-14 10:08:38 +0100

    Update design

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -43,10 +43,10 @@ android {
 }
 
 dependencies {
-    implementation("io.didomi.sdk:android:1.86.1")
+    implementation("io.didomi.sdk:android:1.86.3")
 
     implementation("androidx.core:core-ktx:1.9.0")
 
-    implementation("com.google.android.gms:play-services-ads:21.5.0")
+    implementation("com.google.android.gms:play-services-ads:22.5.0")
     implementation("com.google.android.material:material:1.8.0")
 }
