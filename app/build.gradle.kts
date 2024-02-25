a72a0e1077b28ddf2aa58f5e1e650c083a401ab4
Nicolas CHAIX <nicolas.chaix@didomi.io>
2024-02-25 00:15:10 +0000

    Update Didomi SDK dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,7 +42,7 @@ android {
 }
 
 dependencies {
-    implementation("io.didomi.sdk:android:2.0.0")
+    implementation("io.didomi.sdk:android:2.0.1")
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("com.google.android.gms:play-services-ads:22.6.0")
     implementation("com.google.android.material:material:1.11.0")
