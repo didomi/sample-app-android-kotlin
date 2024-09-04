9fdb1d1b6602a5bfa94e3558d993862779572d9c
Nicolas CHAIX <nicolas.chaix@didomi.io>
2024-09-04 10:51:20 +0200

    update AGP and plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,9 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
-    id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.10" apply false
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.21" apply false
 }
 
 tasks.register("clean", Delete::class) {
-    delete(rootProject.buildDir, "buildSrc/build")
+    delete(rootProject.layout.buildDirectory, "buildSrc/build")
 }
