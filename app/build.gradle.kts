6a6739ed59e36e42d3268009ed690285e8db696e
Nicolas CHAIX <80761828+nicolas-chaix-didomi@users.noreply.github.com>
2024-09-04 19:55:12 +0200

    Update build config and scripts (#12)
    * update AGP and plugin
    
    * update gradle wrapper
    
    * update dependencies
    
    * fix scripts updating Didomi SDK dependency
    
    * update workflows actions

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -42,8 +42,8 @@ android {
 }
 
 dependencies {
-    implementation("io.didomi.sdk:android:2.9.0")
-    implementation("androidx.core:core-ktx:1.12.0")
-    implementation("com.google.android.gms:play-services-ads:22.6.0")
-    implementation("com.google.android.material:material:1.11.0")
+    implementation("io.didomi.sdk:android:2.11.0")
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("com.google.android.gms:play-services-ads:23.3.0")
+    implementation("com.google.android.material:material:1.12.0")
 }
